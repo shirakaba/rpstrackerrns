336dd2439aac5b5d69e62429bea9b380c45cf113
Alexander Ziskind <alex@nuvious.com>
2019-07-26 12:50:57 -0400

    upgrades to 6

--- app/utils/status-bar/status-bar-utils.ios.ts
+++ app/utils/status-bar/status-bar-utils.ios.ts
@@ -4,9 +4,11 @@ declare const UIApplication: any;
 declare const UIStatusBarStyle: any;
 
 export function setStatusBarColors() {
+    /*
     utils.ios.getter(
         UIApplication,
         UIApplication.sharedApplication
     ).statusBarStyle =
         UIStatusBarStyle.LightContent;
+        */
 }
