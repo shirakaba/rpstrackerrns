15752cfe7e0ec2fd2f68a53e6b39ebd2e506bf1b
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-07-27 16:45:07 +0100

    More work on LoginPage

--- app/views/pages/login/LoginPage.tsx
+++ app/views/pages/login/LoginPage.tsx
@@ -1,30 +1,40 @@
 import * as React from "react";
-import { $TabView, $TabViewItem, $StackLayout, $Label, $Page, $GridLayout, $Image, $ActivityIndicator, $TextField } from "react-nativescript";
+import { $TabView, $TabViewItem, $StackLayout, $Label, $Page, $GridLayout, $Image, $ActivityIndicator, $TextField, $Button } from "react-nativescript";
 import { ItemSpec } from "tns-core-modules/ui/layouts/grid-layout/grid-layout";
 import { isIOS, isAndroid } from "tns-core-modules/platform/platform";
 import { Page, Color } from "react-nativescript/dist/client/ElementRegistry";
+import { PtAuthService } from '~/core/contracts/services';
+import { getAuthService } from '~/globals/dependencies/locator';
 
 interface State {
-    loggingIn: boolean,
     email: string,
-    emailEmpty: boolean,
     emailValid: boolean,
+    emailEmpty: boolean,
+    password: string,
+    passwordEmpty: boolean,
+    formValid: boolean,
+    loggingIn: boolean,
 }
 
 export class LoginPage extends React.Component<{ forwardedRef: React.RefObject<Page> }, State> {
+    private readonly authService: PtAuthService = getAuthService();
+
     constructor(props){
         super(props);
 
         this.state = {
+            password: 'nuvious',
+            passwordEmpty: false,
             emailEmpty: false,
             emailValid: true,
             email: "alex@nuvious.com",
             loggingIn: false,
+            formValid: true,
         };
     }
 
     render(){
-        const { loggingIn, emailEmpty, email, emailValid } = this.state;
+        const { loggingIn, emailEmpty, email, emailValid, password, passwordEmpty, formValid } = this.state;
 
         return (
             <$Page ref={this.props.forwardedRef} actionBarHidden={true}>
@@ -49,24 +59,47 @@ export class LoginPage extends React.Component<{ forwardedRef: React.RefObject<P
                             <$Label className="title" row={0}>L('Login') would go in here</$Label>
 
                             <$StackLayout row={1}>
-                                <$StackLayout className="input-field with-validation">
-                                    <$GridLayout
-                                        rows={[]}
-                                        columns={[new ItemSpec(0, "star"), new ItemSpec(25, "pixel")]}
-                                        className={"login-field-wrapper"}
-                                    >
-                                        {/* TODO: TextField */}
-                                        <$TextField
-                                            col={0}
-                                            className={!emailEmpty && emailValid ? "login-field valid" : "login-field invalid" }
-                                            text={email}
-                                        />
-                                        <$Label
-                                            col={1}
-                                            className="fa login-icon"
-                                            text={`TODO: unescape &#xf0e0;`}
-                                        />
-                                    </$GridLayout>
+                                <$StackLayout>
+                                    <$StackLayout className="input-field with-validation">
+                                        <$GridLayout
+                                            rows={[]}
+                                            columns={[new ItemSpec(0, "star"), new ItemSpec(25, "pixel")]}
+                                            className={"login-field-wrapper"}
+                                        >
+                                            <$TextField
+                                                col={0}
+                                                className={!emailEmpty && emailValid ? "login-field valid" : "login-field invalid" }
+                                                hint={"Email"}
+                                                keyboardType={"email"}
+                                                text={email}
+                                            />
+                                            <$Label col={1} className="fa login-icon" text="&#xf0e0;"/>
+                                        </$GridLayout>
+
+                                        <$StackLayout className="login-field-validation-wrapper">
+                                            <$Label visibility={ emailEmpty ? 'visible' : 'collapse' } text="email is missing" className="validation"/>
+                                            <$Label visibility={ !emailValid ? 'visible' : 'collapse' } text="the email is not in the correct format" className="validation"/>
+                                        </$StackLayout>
+                                    </$StackLayout>
+
+                                    <$StackLayout className="input-field with-validation">
+                                        <$Label col={1} className={"fa login-icon"} text="&#xf023;"/>
+
+                                        <$GridLayout rows={[]} columns={[new ItemSpec(0, "star"), new ItemSpec(25, "pixel")]} className="login-field-wrapper">
+                                            <$TextField col={0} className={ !passwordEmpty ? 'login-field valid' : 'login-field invalid' } hint="Password" secure={true} text={password}/>
+                                            <$Label col={1} className="fa login-icon" text="&#xf023;"/>
+                                        </$GridLayout>
+
+                                        <$StackLayout>
+                                            <$StackLayout>
+                                                <$Label visibility={ passwordEmpty ? 'visible' : 'collapse' } text="password is required" className="validation"/>
+                                            </$StackLayout>
+                                        </$StackLayout>
+                                    </$StackLayout>
+
+                                    <$Button text="{{ L('Login') }}" onTap={this.onLoginTap} isEnabled={ formValid } className={ formValid ? 'btn-login btn-primary' : 'btn-login' }/>
+
+                                    <$Label className="hr"/>
                                 </$StackLayout>
                             </$StackLayout>
 
@@ -91,5 +124,9 @@ export class LoginPage extends React.Component<{ forwardedRef: React.RefObject<P
 
     private readonly onGotoRegisterTap = () => {
         console.log(`STUB: onGotoRegisterTap()`);
+    };
+
+    private readonly onLoginTap = () => {
+        console.log(`STUB: onLoginTap()`);
     };
 }
