20edee0ca56a74e8cb4f4a76a338d12ac75550ce
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-07-28 17:35:10 +0100

    Provisional drawerContent

--- app/views/pages/backlog/BacklogPage.tsx
+++ app/views/pages/backlog/BacklogPage.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { $StackLayout, $Label, $Page, $GridLayout, $Image, $ActionBar, $ActionItem } from "react-nativescript";
+import { $StackLayout, $Label, $Page, $GridLayout, $Image, $ActionBar, $ActionItem, $Button } from "react-nativescript";
 import { $RadSideDrawer, mainContentNodeTreeRole, drawerContentNodeTreeRole } from "~/rns-plugins/RadSideDrawer";
 import { ItemSpec } from "tns-core-modules/ui/layouts/grid-layout/grid-layout";
 import { isIOS, isAndroid } from "tns-core-modules/platform/platform";
@@ -7,7 +7,12 @@ import { Page } from "react-nativescript/dist/client/ElementRegistry";
 import { SideDrawerLocation, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 
 export class BackLogPage extends React.Component<{ forwardedRef: React.RefObject<Page> }, {}> {
+    componentDidMount(){
+        console.log(`BackLogPage.componentDidMount`);
+    }
+
     render(){
+        console.log(`BackLogPage.render`);
         return (
             <$Page ref={this.props.forwardedRef}>
                 <$ActionBar>
@@ -17,18 +22,38 @@ export class BackLogPage extends React.Component<{ forwardedRef: React.RefObject
                         </$StackLayout>
                     </$ActionItem>
                 </$ActionBar>
-                <$RadSideDrawer id={"sideDrawer"} drawerLocation={SideDrawerLocation.Right} drawerTransition={new SlideInOnTopTransition()}>
+                <$RadSideDrawer id={"sideDrawer"} drawerLocation={SideDrawerLocation.Right}>
                     <$Label>You shouldn't see me</$Label>
                     <$StackLayout __rns__nodeTreeRole={mainContentNodeTreeRole}>
                         <$Label>I'm the main content</$Label>
                     </$StackLayout>
                     <$StackLayout __rns__nodeTreeRole={drawerContentNodeTreeRole}>
-                        <$Label>I'm the drawer content</$Label>
+                        <$GridLayout
+                            className="side-drawer-panel"
+                            rows={[new ItemSpec(1, "auto"), new ItemSpec(1, "star"), new ItemSpec(100, "pixel")]}
+                            columns={[]}
+                        >
+                            <$StackLayout row={0}>
+                                <$Image src="res://rpslogo" className="slide-out-img" stretch="aspectFit" />
+                            </$StackLayout>
+
+                            <$StackLayout row={1} className="menu-container">
+                                {/* <menu:menu presetSelected="{{ onPresetSelected }}"/> */}
+                            </$StackLayout>
+
+                            <$StackLayout row={2} className="menu-container">
+                                <$Button onTap={this.onLogoutTap} text="LOGOUT" className="btn-inverse" />
+                            </$StackLayout>
+                        </$GridLayout>
                     </$StackLayout>
                 </$RadSideDrawer>
             </$Page>
         );
     }
+
+    private readonly onLogoutTap = () => {
+        // toggleDrawer
+    };
 
     private readonly onToggleDrawerTap = () => {
         // toggleDrawer
