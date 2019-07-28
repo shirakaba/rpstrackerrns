011fe0efc24c27e0cccbb224efc92a17b2ad50ee
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-07-28 16:57:21 +0100

    Provisonal RadSideDrawer usage

--- app/views/pages/backlog/BacklogPage.tsx
+++ app/views/pages/backlog/BacklogPage.tsx
@@ -1,15 +1,36 @@
 import * as React from "react";
-import { $StackLayout, $Label, $Page, $GridLayout, $Image } from "react-nativescript";
+import { $StackLayout, $Label, $Page, $GridLayout, $Image, $ActionBar, $ActionItem } from "react-nativescript";
+import { $RadSideDrawer, mainContentNodeTreeRole, drawerContentNodeTreeRole } from "~/rns-plugins/RadSideDrawer";
 import { ItemSpec } from "tns-core-modules/ui/layouts/grid-layout/grid-layout";
 import { isIOS, isAndroid } from "tns-core-modules/platform/platform";
 import { Page } from "react-nativescript/dist/client/ElementRegistry";
+import { SideDrawerLocation, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 
 export class BackLogPage extends React.Component<{ forwardedRef: React.RefObject<Page> }, {}> {
     render(){
         return (
-            <$Page ref={this.props.forwardedRef} actionBarHidden={true}>
-                <$Label>Why oh why did you have to use RadSideDrawer</$Label>
+            <$Page ref={this.props.forwardedRef}>
+                <$ActionBar>
+                    <$ActionItem>
+                        <$StackLayout className="navbar_image_wrapper" horizontalAlignment={ isIOS ? "right" : void 0 }>
+                            <$Image src="res://iconelipseswhite" onTap={this.onToggleDrawerTap} />
+                        </$StackLayout>
+                    </$ActionItem>
+                </$ActionBar>
+                <$RadSideDrawer id={"sideDrawer"} drawerLocation={SideDrawerLocation.Right} drawerTransition={new SlideInOnTopTransition()}>
+                    <$Label>You shouldn't see me</$Label>
+                    <$StackLayout __rns__nodeTreeRole={mainContentNodeTreeRole}>
+                        <$Label>I'm the main content</$Label>
+                    </$StackLayout>
+                    <$StackLayout __rns__nodeTreeRole={drawerContentNodeTreeRole}>
+                        <$Label>I'm the drawer content</$Label>
+                    </$StackLayout>
+                </$RadSideDrawer>
             </$Page>
         );
     }
+
+    private readonly onToggleDrawerTap = () => {
+        // toggleDrawer
+    };
 }
