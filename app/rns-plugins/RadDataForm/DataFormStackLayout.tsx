5a2822d566c246d3031b4f0919ecdfc76f9d4063
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-08-05 20:53:54 +0100

    DataFormStackLayout

---
+++ app/rns-plugins/RadDataForm/DataFormStackLayout.tsx
@@ -0,0 +1,83 @@
+import * as console from "react-nativescript/dist/shared/Logger";
+import * as React from "react";
+import { DataFormStackLayout as NativeScriptDataFormStackLayout } from "nativescript-ui-dataform";
+import { PropsWithoutForwardedRef } from "react-nativescript/dist/shared/NativeScriptComponentTypings";
+import { DataFormLayoutComponentProps, _DataFormLayout } from "./DataFormLayout";
+import { register } from "react-nativescript/dist/client/ElementRegistry";
+import { Container, HostContext, Instance } from "react-nativescript/dist/shared/HostConfigTypes";
+
+const elementKey: string = "radDataFormDataFormStackLayout";
+register(
+    elementKey,
+    (
+        props: Props,
+        rootContainerInstance: Container,
+        hostContext: HostContext,
+    ) => {
+        return new NativeScriptDataFormStackLayout();
+    }
+);
+
+type DataFormStackLayoutProps = Pick<NativeScriptDataFormStackLayout,
+"orientation"
+>;
+
+interface Props {
+}
+
+export type DataFormStackLayoutComponentProps<
+    E extends NativeScriptDataFormStackLayout = NativeScriptDataFormStackLayout
+> = Props /* & typeof RCTDataFormStackLayout.defaultProps */ & Partial<DataFormStackLayoutProps> & DataFormLayoutComponentProps<E>;
+
+/**
+ * A React wrapper around the NativeScript DataFormStackLayout component.
+ *
+ * See: ui/action-bar/action-bar
+ * See: https://docs.nativescript.org/ui/action-bar#action-items
+ * See: https://github.com/NativeScript/nativescript-sdk-examples-js/tree/master/app/ns-ui-widgets-category/action-bar/items-actionbar
+ */
+export class _DataFormStackLayout<
+    P extends DataFormStackLayoutComponentProps<E>,
+    S extends {},
+    E extends NativeScriptDataFormStackLayout
+> extends _DataFormLayout<P, S, E> {
+
+    render() {
+        const {
+            forwardedRef,
+
+            onPropertyChange,
+
+            children,
+            ...rest
+        } = this.props;
+
+        return React.createElement(
+            elementKey,
+            {
+                ...rest,
+                ref: forwardedRef || this.myRef,
+            },
+            children
+        );
+    }
+}
+
+type OwnPropsWithoutForwardedRef = PropsWithoutForwardedRef<DataFormStackLayoutComponentProps<NativeScriptDataFormStackLayout>>;
+
+export const DataFormStackLayout: React.ComponentType<
+    OwnPropsWithoutForwardedRef & React.ClassAttributes<NativeScriptDataFormStackLayout>
+> = React.forwardRef<NativeScriptDataFormStackLayout, OwnPropsWithoutForwardedRef>(
+    (props: React.PropsWithChildren<OwnPropsWithoutForwardedRef>, ref: React.RefObject<NativeScriptDataFormStackLayout>) => {
+        const { children, ...rest } = props;
+
+        return React.createElement(
+            _DataFormStackLayout,
+            {
+                ...rest,
+                forwardedRef: ref,
+            },
+            children
+        );
+    }
+);
