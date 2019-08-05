6a7b13e28e636cab4674895a43959dd335d111af
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-08-05 20:19:34 +0100

    Provisional PropertyEditor

---
+++ app/rns-plugins/RadSideDrawer/PropertyEditor.tsx
@@ -0,0 +1,85 @@
+import * as console from "react-nativescript/dist/shared/Logger";
+import * as React from "react";
+import { PropertyEditor as NativeScriptPropertyEditor } from "nativescript-ui-dataform";
+import { PropsWithoutForwardedRef } from "react-nativescript/dist/shared/NativeScriptComponentTypings";
+import { ViewBaseComponentProps, RCTViewBase } from "react-nativescript/dist/components/ViewBase";
+import { register } from "react-nativescript/dist/client/ElementRegistry";
+import { Container, HostContext, Instance } from "react-nativescript/dist/shared/HostConfigTypes";
+
+const elementKey: string = "radDataFormPropertyEditor";
+register(
+    elementKey,
+    (
+        props: Props,
+        rootContainerInstance: Container,
+        hostContext: HostContext,
+    ) => {
+        return new NativeScriptPropertyEditor();
+    }
+);
+
+type PropertyEditorProps = Pick<NativeScriptPropertyEditor,
+"propertyEditorStyle"|
+"type"|
+"params">;
+
+interface Props {
+}
+
+export type PropertyEditorComponentProps<
+    E extends NativeScriptPropertyEditor = NativeScriptPropertyEditor
+> = Props /* & typeof RCTPropertyEditor.defaultProps */ & Partial<PropertyEditorProps> & ViewBaseComponentProps<E>;
+
+/**
+ * A React wrapper around the NativeScript PropertyEditor component.
+ *
+ * See: ui/action-bar/action-bar
+ * See: https://docs.nativescript.org/ui/action-bar#action-items
+ * See: https://github.com/NativeScript/nativescript-sdk-examples-js/tree/master/app/ns-ui-widgets-category/action-bar/items-actionbar
+ */
+export class _PropertyEditor<
+    P extends PropertyEditorComponentProps<E>,
+    S extends {},
+    E extends NativeScriptPropertyEditor
+> extends RCTViewBase<P, S, E> {
+
+    render() {
+        const {
+            forwardedRef,
+
+            onPropertyChange,
+
+            children,
+            // actionView, /* We disallow this at the typings level. */
+            ...rest
+        } = this.props;
+
+        return React.createElement(
+            elementKey,
+            {
+                ...rest,
+                ref: forwardedRef || this.myRef,
+            },
+            children
+        );
+    }
+}
+
+type OwnPropsWithoutForwardedRef = PropsWithoutForwardedRef<PropertyEditorComponentProps<NativeScriptPropertyEditor>>;
+
+export const PropertyEditor: React.ComponentType<
+    OwnPropsWithoutForwardedRef & React.ClassAttributes<NativeScriptPropertyEditor>
+> = React.forwardRef<NativeScriptPropertyEditor, OwnPropsWithoutForwardedRef>(
+    (props: React.PropsWithChildren<OwnPropsWithoutForwardedRef>, ref: React.RefObject<NativeScriptPropertyEditor>) => {
+        const { children, ...rest } = props;
+
+        return React.createElement(
+            _PropertyEditor,
+            {
+                ...rest,
+                forwardedRef: ref,
+            },
+            children
+        );
+    }
+);
